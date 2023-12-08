0f85d093b9c032c31fdae2093446100a670100c0
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-08 10:40:52 +0800

    added model package

---
+++ src/main/java/dave/dev/eventmanagement/model/Event.java
@@ -0,0 +1,50 @@
+package dave.dev.eventmanagement.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String eventName;
+
+    @Column(nullable = false)
+    private LocalDateTime eventDateTime;
+
+    public Event(Long id, String eventName, LocalDateTime eventDateTime) {
+        this.id = id;
+        this.eventName = eventName;
+        this.eventDateTime = eventDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public LocalDateTime getEventDateTime() {
+        return eventDateTime;
+    }
+
+    public void setEventDateTime(LocalDateTime eventDateTime) {
+        this.eventDateTime = eventDateTime;
+    }
+}
