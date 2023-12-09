71a5234171986f08e65e187b1c3a7d9104e3ab11
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-09 16:32:01 +0800

    added constructors, getters, and setters

--- src/main/java/dave/dev/eventmanagement/model/Rsvp.java
+++ src/main/java/dave/dev/eventmanagement/model/Rsvp.java
@@ -9,4 +9,53 @@ public class Rsvp {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @Column(nullable = false)
+    private String attendeeName;
+
+    @Column(nullable = false)
+    private boolean attending;
+
+    public Rsvp(Long id, Event event, String attendeeName, boolean attending) {
+        this.id = id;
+        this.event = event;
+        this.attendeeName = attendeeName;
+        this.attending = attending;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public String getAttendeeName() {
+        return attendeeName;
+    }
+
+    public void setAttendeeName(String attendeeName) {
+        this.attendeeName = attendeeName;
+    }
+
+    public boolean isAttending() {
+        return attending;
+    }
+
+    public void setAttending(boolean attending) {
+        this.attending = attending;
+    }
 }
