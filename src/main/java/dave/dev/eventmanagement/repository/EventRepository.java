6f7996e742726818f9cb91b934ba9f5669ab80a6
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-11 13:14:47 +0800

    added event repository

---
+++ src/main/java/dave/dev/eventmanagement/repository/EventRepository.java
@@ -0,0 +1,9 @@
+package dave.dev.eventmanagement.repository;
+
+import dave.dev.eventmanagement.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}
