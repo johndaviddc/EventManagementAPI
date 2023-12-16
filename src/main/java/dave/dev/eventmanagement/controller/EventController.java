a21a4837a0a44c82e9818a287a352ae6407ac886
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-16 16:38:54 +0800

    added controller package

---
+++ src/main/java/dave/dev/eventmanagement/controller/EventController.java
@@ -0,0 +1,17 @@
+package dave.dev.eventmanagement.controller;
+
+import dave.dev.eventmanagement.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+    private final EventService eventService;
+
+    @Autowired
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+}
