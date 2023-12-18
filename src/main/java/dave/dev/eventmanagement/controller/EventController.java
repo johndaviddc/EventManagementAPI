d0ebc0998b9d4cffc4c9d9e5d9328cc3d01b7d88
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-18 09:29:27 +0800

    added get mapping

--- src/main/java/dave/dev/eventmanagement/controller/EventController.java
+++ src/main/java/dave/dev/eventmanagement/controller/EventController.java
@@ -5,10 +5,7 @@ import dave.dev.eventmanagement.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/events")
@@ -24,5 +21,11 @@ public class EventController {
     public ResponseEntity<Event> createEvent(@RequestBody Event event) {
         Event createdEvent = eventService.createEvent(event);
         return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long eventId) {
+        Event event = eventService.getEventById(eventId);
+        return new ResponseEntity<>(event, HttpStatus.OK);
     }
 }
