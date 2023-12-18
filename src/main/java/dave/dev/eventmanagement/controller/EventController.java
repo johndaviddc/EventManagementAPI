edb295fa10a997ff339c9b80d55a1316ab588375
John David Dela Cruz <johndaviddc1@gmail.com>
2023-12-18 09:08:04 +0800

    added post mapping

--- src/main/java/dave/dev/eventmanagement/controller/EventController.java
+++ src/main/java/dave/dev/eventmanagement/controller/EventController.java
@@ -1,7 +1,12 @@
 package dave.dev.eventmanagement.controller;
 
+import dave.dev.eventmanagement.model.Event;
 import dave.dev.eventmanagement.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,11 @@ public class EventController {
     @Autowired
     public EventController(EventService eventService) {
         this.eventService = eventService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Event> createEvent(@RequestBody Event event) {
+        Event createdEvent = eventService.createEvent(event);
+        return new ResponseEntity<>(createdEvent, HttpStatus.CREATED);
     }
 }
